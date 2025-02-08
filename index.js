01c7ad77fe66955819237ef3b3f8c5fd6c8d2f3a
Natovarsilva <natalas.4994@gmail.com>
2025-02-08 13:57:33 -0500

    proyecto 7 primera entrega

---
+++ index.js
@@ -0,0 +1,29 @@
+let editButton = document.querySelector(".profile__info_edit-button");
+let popup = document.querySelector(".popup");
+let formInputName = document.querySelector(".form__text-input_name");
+let formInputDesc = document.querySelector(".form__text-input_description");
+let profileName = document.querySelector(".profile__info_name");
+let profileDesc = document.querySelector(".profile__info_description");
+let formProfile = document.querySelector(".form");
+let popupCloseButton = document.querySelector(".popup__close");
+
+function handleOpenPopup() {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function handleChangeProfile(evt) {
+  evt.preventDefault();
+  profileName.textContent = formInputName.value;
+  profileDesc.textContent = formInputDesc.value;
+  popup.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", handleOpenPopup);
+formProfile.addEventListener("submit", handleChangeProfile);
+popupCloseButton.addEventListener("click", function () {
+  closePopup();
+});
